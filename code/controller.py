7c81d7f00820e2beb61b157e89684befb9a68813
Haidyn <haidyn1988@gmail.com>
2017-06-01 21:42:18 +1000

    fix D gain

--- code/controller.py
+++ code/controller.py
@@ -5,7 +5,7 @@ class PID:
     def __init__(self, Kp, Ki, Kd):
         self.Kp = Kp
         self.Ki = Ki
-        self.Kp = Kp
+        self.Kd = Kd
         self.set_point = 0.
         self.error = 0.
         self.integral = 0.
@@ -26,9 +26,7 @@ class PID:
 
         # integral error
         self.integral += self.error
-
         # derivative error
         delta_error = self.old_error - measurement
         self.old_error = measurement
-
-        return self.Kp * self.error + self.Ki * self.integral + self.Kp * delta_error
+        return self.Kp * self.error + self.Ki * self.integral + self.Kd * delta_error
